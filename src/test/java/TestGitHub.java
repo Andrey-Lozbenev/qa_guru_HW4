f11147b159b0e429d8062747623c930b2dacdbd0
Andrey <hanom_dzr@mail.ru>
2023-03-14 15:43:06 +0300

    deleting extra lines

--- src/test/java/TestGitHub.java
+++ src/test/java/TestGitHub.java
@@ -10,27 +10,14 @@ public class TestGitHub {
 
     @BeforeAll
     static void beforeAll() {
-
         Configuration.browserSize = "1920x1080";
     }
-
         @Test
     void testJunit5(){
-            //Откройте страницу Selenide в Github
         open("https://github.com/selenide/selenide");
-            // Перейдите в раздел Wiki проекта
         $("#wiki-tab").click();
-            //Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
         $(".js-wiki-more-pages-link").click();
         $("[data-filterable-for=wiki-pages-filter]").$(byText("SoftAssertions")).click();
-            //Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
         $("#wiki-wrapper").shouldHave(text("Using JUnit5 extend test class"));
-
-
-
     }
-
-
-
-
 }
